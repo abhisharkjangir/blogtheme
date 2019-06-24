484332e0aade37494f6763081b4127a4722e3405
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2019-06-24 21:45:42 +0530

    deploy fixes

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts("/precache-manifest.335b84174dc802938c52c952417a4534.js");
+importScripts(
+  "/precache-manifest.76dae6d3eaedc8ed308fb430f7364676.js"
+);
 
 workbox.clientsClaim();
 
@@ -29,5 +29,6 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
-  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -12,8 +12,8 @@ import AppReducer, {
 } from "./app/containers/app/appReducer";
 
 // Home Reducer
-import { HOME_STATE_KEY } from "./app/containers/pages/home/HomeConstants";
-import HomeReducer from "./app/containers/pages/home/HomeReducer";
+import { HOME_STATE_KEY } from "./app/containers/pages/home/homeConstants";
+import HomeReducer from "./app/containers/pages/home/homeReducer";
 
 // Loader Reducer
 import { LOADER_STATE_KEY } from "./app/components/common/loader/loaderConstants";
