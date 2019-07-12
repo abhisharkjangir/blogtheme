be4a70a538996cfbcf2831072afa23122e993581
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2019-07-13 00:03:29 +0530

    build

--- build/precache-manifest.39d100908bee4bf4f778b7f72af4f533.js
+++ build/precache-manifest.39d100908bee4bf4f778b7f72af4f533.js
@@ -1,50 +1,50 @@
 self.__precacheManifest = [
   {
-    revision: "c0eaeae376757107bae9",
-    url: "/static/js/notfound.b271cae3.chunk.js"
+    "revision": "c0eaeae376757107bae9",
+    "url": "/static/js/notfound.b271cae3.chunk.js"
   },
   {
-    revision: "7b851cc327438e69a419",
-    url: "/static/js/0.f3d6f7fa.chunk.js"
+    "revision": "7b851cc327438e69a419",
+    "url": "/static/js/0.f3d6f7fa.chunk.js"
   },
   {
-    revision: "7c3a5904ba1d87ed2242",
-    url: "/static/js/home.8b4a43ed.chunk.js"
+    "revision": "7c3a5904ba1d87ed2242",
+    "url": "/static/js/home.8b4a43ed.chunk.js"
   },
   {
-    revision: "0a0bf31719468e80b4f6",
-    url: "/static/js/6.c3847da5.chunk.js"
+    "revision": "0a0bf31719468e80b4f6",
+    "url": "/static/js/6.c3847da5.chunk.js"
   },
   {
-    revision: "f5e5d8d18b3bdb9f9364",
-    url: "/static/js/main.8aecface.chunk.js"
+    "revision": "f5e5d8d18b3bdb9f9364",
+    "url": "/static/js/main.8aecface.chunk.js"
   },
   {
-    revision: "7998b9d3cb796afd6ad8",
-    url: "/static/js/5.2ea4983c.chunk.js"
+    "revision": "7998b9d3cb796afd6ad8",
+    "url": "/static/js/5.2ea4983c.chunk.js"
   },
   {
-    revision: "1309db9727771b0a9fc8",
-    url: "/static/js/runtime~main.c9ce408b.js"
+    "revision": "1309db9727771b0a9fc8",
+    "url": "/static/js/runtime~main.c9ce408b.js"
   },
   {
-    revision: "c0eaeae376757107bae9",
-    url: "/static/css/notfound.43f216be.chunk.css"
+    "revision": "c0eaeae376757107bae9",
+    "url": "/static/css/notfound.43f216be.chunk.css"
   },
   {
-    revision: "f5e5d8d18b3bdb9f9364",
-    url: "/static/css/main.4b3868f9.chunk.css"
+    "revision": "f5e5d8d18b3bdb9f9364",
+    "url": "/static/css/main.4b3868f9.chunk.css"
   },
   {
-    revision: "7c3a5904ba1d87ed2242",
-    url: "/static/css/home.500f8870.chunk.css"
+    "revision": "7c3a5904ba1d87ed2242",
+    "url": "/static/css/home.500f8870.chunk.css"
   },
   {
-    revision: "7998b9d3cb796afd6ad8",
-    url: "/static/css/5.35b93edc.chunk.css"
+    "revision": "7998b9d3cb796afd6ad8",
+    "url": "/static/css/5.35b93edc.chunk.css"
   },
   {
-    revision: "1aa49efc9a51a5ba9db8b8e61e2b5aaf",
-    url: "/index.html"
+    "revision": "1aa49efc9a51a5ba9db8b8e61e2b5aaf",
+    "url": "/index.html"
   }
 ];
